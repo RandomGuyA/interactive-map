947024b5dd1acf835b9fd4edebcba16617bc003d
byron <Byron@atebol.com>
2017-07-25 10:41:19 +0100

    update

--- scripts/jquery.plugin.interactive-map.js
+++ scripts/jquery.plugin.interactive-map.js
@@ -1,7 +1,6 @@
 ;(function ($, doc, win) {
     "use strict";
 
-
     //--------------------------------------//
     //              GLOBALS
     //--------------------------------------//
@@ -12,7 +11,6 @@
 
     $.interactive_map_builder = function (element, options) {
 
-
         //--------------------------------------//
         //       DEFAULT PLUGIN SETTINGS
         //--------------------------------------//
@@ -20,10 +18,13 @@
         var defaults = {
 
             dropdown: true,
-            title: "Percentage of welsh speakers per county in Wales"
+            title: "Percentage of welsh speakers per county in Wales",
+            DIRECTORY: "",
+            key_container: $('#interactive-map'),
+            add_labels: false,
+            nav_type: 'slider'   // 'silder':'dropdown':'none'
 
         };
-
 
         //--------------------------------------//
         //       PLUGIN PRIVATE VARIABLES
@@ -31,6 +32,7 @@
 
         var plugin = this; //use plugin instead of "this"
         var id = ID;  //set unique ID for plugin instance
+        var config;
 
         var files_loaded = 0;
         var file_count = 4;
@@ -45,10 +47,11 @@
         var current_year;
         var $map;
         var $interface;
+        var year_list = [1951, 1961, 1971, 1981, 1991, 2001, 2011];
+
         //--------------------------------------//
         //       CUSTOM SETTING SETUP
         //--------------------------------------//
-
 
         plugin.settings = {}; //initialise empty settings object
 
@@ -68,20 +71,14 @@
 
             // the plugin's final properties are the merged default and user-provided options (if any)
             plugin.settings = $.extend({}, defaults, options, meta);
-
+            config = plugin.settings;
             console.log("initialised plugin " + name + " -- " + id);
 
-            $element.append(
-                $('<div>').attr('id', 'interface'),
-                $('<div>').attr('id', 'map')
-            );
-            $map = $('#map');
-
-            load_file('data/xml/welsh_counties.svg');
-            load_csv('data/csv/year_data.csv');
-            load_csv('data/csv/county.csv');
-            load_csv('data/csv/county_to_year.csv');
-
+            setup_divs();
+            load_file(config.DIRECTORY + 'data/xml/welsh_counties.svg');
+            load_csv(config.DIRECTORY + 'data/csv/year_data.csv');
+            load_csv(config.DIRECTORY + 'data/csv/county.csv');
+            load_csv(config.DIRECTORY + 'data/csv/county_to_year.csv');
         };
 
 
@@ -95,10 +92,11 @@
          *  element.data('pluginName').publicMethod(arg1, arg2, ... argn) from outside the plugin, where "element"
          *  is the element the plugin is attached to;
          */
-
-        plugin.foo_public_method = function () {
-
-            // code goes here
+        plugin.update_map = function (year) {
+            set_year(iterate_and_compare(map_data, {year: get_census_year(year)}));
+            render_map(current_year);
+            setup_labels(current_year);
+            setup_key();
 
         };
 
@@ -106,23 +104,43 @@
         //--------------------------------------//
         //              PRIVATE METHODS
         //--------------------------------------//
+
         /**
          *  these methods can be called only from inside the plugin like:
          *  methodName(arg1, arg2, ... argn)
          */
-
         var setup_map = function () {
-            console.log("here");
+
+            $map.append(
+                $('<div>').attr('id', 'label-wrapper').append(
+                    $('<ul>').attr('id', 'path-labels')
+                )
+            );
             setup_data();
-            setup_interface();
+            add_map_nav();
             set_year(map_data[0]);
             render_map(current_year);
+            setup_labels(current_year);
             setup_key();
-            label_adjustment();
+            label_adjustment(current_year);
+        };
+
+        var setup_divs = function () {
+
+            $element.append(
+                $('<div>').attr('id', 'interface'),
+                $('<div>').attr('id', 'map')
+            );
+            $map = $('#map');
+
         };
 
         var set_year = function (year) {
             current_year = year;
+        };
+
+        var get_census_year = function (year) {
+            return ((Math.floor(year / 10) * 10) + 1);
         };
 
         var get_year_by_id = function (id) {
@@ -145,12 +163,9 @@
 
             $('path').click_county();
 
-            var counties = year.locations;
             $('.path-label').hide();
+            iterate_and_call(setup_county, year.locations);
 
-            for (var a = 0; a < counties.length; a++) {
-                setup_county(counties[a]);
-            }
         };
 
         var hide_layers = function () {
@@ -158,8 +173,26 @@
             $map.find('#layer2').hide();
         };
 
-        var add_dropdown = function () {
+        var add_map_nav = function () {
 
+            switch (config.nav_type) {
+                case 'dropdown':
+                    add_dropdown();
+                    break;
+                case 'slider':
+                    add_slider();
+                    break;
+                default:
+                    break;
+            }
+        };
+
+        var add_slider = function () {
+            $map.prepend(slider());
+            $('#' + 'slider-' + id).slider_event_handler();
+        };
+
+        var add_dropdown = function () {
             if (plugin.settings.dropdown) {
                 $map.append(dropdown());
                 $('#' + 'dropdown-' + id).dropdown_event_handler();
@@ -174,21 +207,37 @@
             );
         };
 
-        var setup_interface = function () {
-            //setup_title();
+        var slider = function () {
 
-            add_dropdown();
-            add_path_labels();
+
+            return $('<div>').addClass('panel rounded').append(
+                $('<div>').addClass('slider-wrapper').append(
+                    $('<input>').attr({
+                        id: 'slider-' + id,
+                        type: 'range',
+                        min: 1951,
+                        max: 2011,
+                        step: 10
+                    }).addClass('slider')
+                )
+            )
         };
 
-        var add_path_labels = function () {
+        var setup_labels = function (year) {
+            $('#path-labels').empty();
+            iterate_and_call(add_county_label, year.locations);
+        };
 
-            $map.prepend($('<div>').attr('id', 'label-wrapper').append(
-                $('<ul>').attr('id', 'path-labels'))
-            );
+        var add_county_label = function (county) {
 
-            for (var a = 0; a < county_data.length; a++) {
-                add_labels(county_data[a]);
+            if (plugin.settings.add_labels) {
+                $('#path-labels').append(
+                    $('<li>')
+                        .addClass('path-label circle')
+                        .attr({id: 'label' + county.id, title: county.welsh})
+                        .text(Math.round(county.value) + "%")
+                        .css(set_css_location(county))
+                );
             }
         };
 
@@ -241,15 +290,10 @@
             return county;
         };
 
-        var get_id_from_string = function(string){
-
+        var get_id_from_string = function (string) {
             var arr = string.split('-');
-            return parseInt(arr[arr.length-1]);
-
-
+            return parseInt(arr[arr.length - 1]);
         };
-
-
 
         var setup_key = function () {
 
@@ -260,30 +304,31 @@
             $('#interface').append(
                 $('<div>').addClass('interface-wrapper').append(
                     $('<div>').addClass('interface-header').append(
-                        $('<h4>').text(current_year.year)
+                        $('<h4>').text(current_year.year + " Census")
                     ),
                     $('<ul>').append(
+                        $('<li>').addClass('top-label active').text("Sir").append(
+                            $('<div>').addClass('left square').append(
+                                $('<div>').addClass('circle').text("%")
+                            ),
+                            $('<div>').addClass('right square').append(
+                                $('<div>').addClass('circle').text())
+                        ),
                         $.map(counties, function (value, index) {
-                            return $('<li>').addClass('side-label').attr('id', "side-label-"+value.id).text(value.welsh).append(
+                            return $('<li>').addClass('side-label').attr('id', "side-label-" + value.id).text(value.welsh).append(
                                 $('<div>').addClass('left square').append(
-                                    $('<div>').addClass('circle').text(value.id)
+                                    $('<div>').addClass('circle').text(Math.round(value.value))
                                 ),
                                 $('<div>').addClass('right square').append(
-                                    $('<div>').addClass('circle').text(Math.round(value.value))
+                                    $('<div>').addClass('circle').text()
                                 )
                             )
                         })
                     )
                 )
             );
+            plugin.settings.key_container.prepend($interface);
             $('.side-label').click_county_label();
-            /*
-             $map.append($('<ul>').attr('id', 'key'));
-             repeat(render, 100);*/
-        };
-
-        var repeat = function (fn, times) {
-            for (var i = 0; i < times; i++) fn(i);
         };
 
         function render(i) {
@@ -297,18 +342,10 @@
             var value = (parseInt(county.value) / 2 * 0.01) + 0.5;
             var color = $.Color(135, 0, 0, value);
             $('#path' + id).css({fill: color});
-            update_label_location(county);
 
-        };
-
-        var add_labels = function (county) {
-
-            $('#path-labels').append(
-                $('<li>')
-                    .addClass('path-label')
-                    .attr({id: 'label' + county[0], title: county[2]})
-                    .text(county[3])
-            );
+            if (plugin.settings.add_labels) {
+                update_label_location(county);
+            }
         };
 
         var update_label_location = function (county) {
@@ -322,10 +359,8 @@
         var set_css_location = function (county) {
 
             var $path = $('#path' + county.path_id);
-
             var top_adjust = -20;
-
-            var label_dimensions = document.querySelector('#label' + county.id).getBoundingClientRect();
+            var label_radius = 15;
             var dimensions = document.querySelector('#' + $path.attr('id')).getBoundingClientRect();
 
             var location = $path.position();
@@ -335,24 +370,23 @@
             var offset_top = county.top / 100 * height;
             var offset_left = county.left / 100 * width;
 
-            var top = offset_top + location.top - (label_dimensions.height / 2) + top_adjust;
-            var left = offset_left + location.left - (label_dimensions.width / 2);
+            var top = offset_top + location.top - label_radius + top_adjust;
+            var left = offset_left + location.left - label_radius;
 
-            /*
-             if(county.id==1){
-             console.log("name " + county.welsh);
-             console.log("location.top: " + location.top);
-             console.log("location.left: " + location.left);
-             console.log("width: " + width);
-             console.log("height: " + height);
-             console.log("offset.top: " + offset_top);
-             console.log("offset.left: " + offset_left);
-             console.log("label_dimensions.height: " + label_dimensions.height);
-             console.log("label_dimensions.width: " + label_dimensions.width);
+            if (county.id == 1) {
+                /*
+                 console.log("name " + county.welsh);
+                 console.log("location.top: " + location.top);
+                 console.log("location.left: " + location.left);
+                 console.log("width: " + width);
+                 console.log("height: " + height);
+                 console.log("offset.top: " + offset_top);
+                 console.log("offset.left: " + offset_left);
+                 console.log("top " + top);
+                 console.log("left " + left);
+                 */
+            }
 
-             console.log("top " + top);
-             console.log("left " + left);
-             }*/
 
             return {
                 top: top,
@@ -361,29 +395,36 @@
         };
 
 
-        var get_county_by_id = function (id){
+        //--------------------------------------//
+        //      HELPER FUNCTIONS
+        //--------------------------------------//
 
-            var county;
-            var counties = current_year.locations;
-            for (var a = 0; a < counties.length; a++) {
-                if (id == counties[a].id) {
-                    county = counties[a];
-                }
-            }
-            return county;
-
+        var iterate_and_call = function (fn, array) {
+            for (var i = 0; i < array.length; i++) fn(array[i]);
         };
 
-        var get_label_id = function (path_id) {
-
-            var id;
-            var counties = current_year.locations;
-            for (var a = 0; a < counties.length; a++) {
-                if (path_id == "path" + counties[a].path_id) {
-                    id = counties[a].id;
+        var iterate_and_compare = function (array, object) {
+            for (var i = 0; i < array.length; i++) {
+                var item = compare(object, array[i]);
+                if (item != undefined) {
+                    return item;
                 }
             }
-            return id;
+        };
+
+        var compare = function (object, item) {
+            var var_name = Object.keys(object)[0];
+            return (object[var_name] == item[var_name]) ? item : null;
+        };
+
+        var get_county_by_id = function (id) {
+            return iterate_and_compare(current_year.locations, {id: id});
+        };
+
+        var get_id_from_path_id = function (path_id) {
+            path_id = parseInt(path_id.replace('path', ''));
+            return iterate_and_compare(current_year.locations, {path_id: path_id});
+
         };
 
         var percent_to_RGB = function (percent) {
@@ -431,7 +472,7 @@
             $.ajax(url)
                 .done(function (response) {
                     data.push(processData(response));
-                    console.log("content loaded");
+                    //console.log("content loaded");
                 })
                 .fail(function (xhr) {
                         alert('Error: ' + xhr.responseText);
@@ -444,7 +485,7 @@
             $.ajax(url)
                 .done(function (response) {
                     $map.html($(response).find('svg'));
-                    console.log("content loaded");
+                    //console.log("content loaded");
                     files_loaded++;
                 })
                 .fail(function (xhr) {
@@ -456,18 +497,15 @@
             function () {
 
                 if (files_loaded >= file_count) {
-                    console.log('all loaded');
+                    //console.log('all loaded');
                     setup_map();
                     clear_interval();
                 }
             }, 100
         );
 
-        var label_adjustment = function () {
-            var counties = current_year.locations;
-            for (var a = 0; a < counties.length; a++) {
-                update_label_location(counties[a]);
-            }
+        var label_adjustment = function (year) {
+            iterate_and_call(update_label_location, year.locations);
         };
 
         var clear_interval = function () {
@@ -483,21 +521,28 @@
          *    These are called by adding the function to the selectors
          *    eg: $('.element).bind_event(args);
          */
-
         $.fn.dropdown_event_handler = function (args) {
 
             $(this).on('change', function () {
                 var id = parseInt($(this).find(':selected').attr('value'));
                 set_year(get_year_by_id(id));
                 render_map(current_year);
-                label_adjustment();
+                setup_labels(current_year);
                 setup_key();
             });
         };
 
+        $.fn.slider_event_handler = function () {
+
+            var $slider = $(this);
+
+            $slider.on('change', function () {
+                plugin.update_map(parseInt($(this).val()));
+            });
+        };
 
         $(window).on('resize', function () {
-            label_adjustment();
+            setup_labels(current_year);
         });
 
         $.fn.click_county = function () {
@@ -505,15 +550,14 @@
             $(this).on('mouseover', function () {
 
                 var path_id = $(this).attr('id');
-                var label_id = get_label_id(path_id);
+                var label = get_id_from_path_id(path_id);
                 $('.side-label').removeClass('active');
-                $('#side-label-' + label_id).addClass('active');
-
+                if (label != undefined) {
+                    $('#side-label-' + label.id).addClass('active');
+                }
             });
             $(this).on('mouseout', function () {
 
-                var path_id = $(this).attr('id');
-                var label_id = get_label_id(path_id);
                 $('.side-label').removeClass('active');
 
             });
@@ -529,8 +573,7 @@
                 var id = get_id_from_string(label.attr('id'));
                 var county = get_county_by_id(id);
 
-                $('#path'+county.path_id).addClass('active');
-
+                $('#path' + county.path_id).addClass('active');
 
 
             });
@@ -576,7 +619,6 @@
 
         plugin.init();
 
-
     };
 
 
@@ -589,7 +631,6 @@
      *  element.data('pluginName').settings.propertyName
      *
      */
-
     $.fn.interactive_map_builder = function (options) {
         return this.each(function () {
             if (undefined == $(this).data(name)) {
@@ -598,4 +639,5 @@
             }
         });
     };
+
 })(jQuery, document, window);
